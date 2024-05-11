fdfbd00e6418ba8c5bbc147d7ccea138f75c3eb2
Prima Hagantha <81164903+primahagantha@users.noreply.github.com>
2024-05-11 10:58:04 +0700

    Update terms.blade.php

--- resources/views/veskai/terms.blade.php
+++ resources/views/veskai/terms.blade.php
@@ -48,82 +48,6 @@
                                         </li>
                                     </ul>
                                 </li>
-                                <!-- <li class="category-item active">
-                                    <a href="#" class="category-title">
-                                        <span>Disclaimer</span><span class="amount">52</span>
-                                    </a>
-                                    <ul class="category-content" style="display: none;">
-                                        <li>
-                                            <a href="#">Fees &amp; Renewals</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Support or Develop</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Availability</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Referral Fee</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="category-item">
-                                    <a href="#" class="category-title">
-                                        <span>Representation</span><span class="amount">13</span>
-                                    </a>
-                                    <ul class="category-content" style="display: none;">
-                                        <li>
-                                            <a href="#">Fees &amp; Renewals</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Support or Develop</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Availability</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Referral Fee</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="category-item">
-                                    <a href="#" class="category-title">
-                                        <span>Referral Fee</span><span class="amount">46</span>
-                                    </a>
-                                    <ul class="category-content" style="display: none;">
-                                        <li>
-                                            <a href="#">Fees &amp; Renewals</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Support or Develop</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Availability</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Referral Fee</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="category-item">
-                                    <a href="#" class="category-title">
-                                        <span>Support or Develop</span><span class="amount">99</span>
-                                    </a>
-                                    <ul class="category-content" style="display: none;">
-                                        <li>
-                                            <a href="#">Fees &amp; Renewals</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Support or Develop</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Availability</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Referral Fee</a>
-                                        </li>
-                                    </ul>
-                                </li>
                             </ul>
                         </div>
                         <div class="widget widget-banner">
@@ -142,7 +66,7 @@
                         </div>
                         <div class="item" id="termsaccount">
                             <h6 class="title">Account</h6>
-                            <p>Semua hal tentang akun yang terdaftar pada Veskai sudah tersetujui oleh pihak user. Pihak Veskai tidak bertanggung jawab bila terjadi sesuatu pada akun kamu karena kesalahan pengguna, mulai dari korban phising, korban scam jual beli antar player, lupa email dan sejenisnya </p>
+                            <p>Pihak Veskai tidak bertanggung jawab bila terjadi sesuatu pada akun kamu karena kesalahan pengguna, mulai dari korban phising, korban scam jual beli, lupa email dan sejenisnya (kecuali jual beli dengan Admin Rocinante sebagai Rekber) </p>
                         </div>
                         <div class="item" id="termspersonaldatainformation">
                             <h6 class="title">Personal Data Information</h6>
@@ -150,12 +74,12 @@
                         </div>
                         <div class="item" id="termsreferralfeeandrefund">
                             <h6 class="title">Referral Fee And Refund</h6>
-                            <p>Refferal Fee yang ada pada Veskai sudah merupakan persetujuan kedua belah pihak Veskai maupun user, dan tidak dapat diubah</p>
-                            <p>Untuk Refund tidak dapat diberlakukan dengan alasan apapun dikarenakan ini termasuk kesalahan user, jikalau merupakan kesalahan pihak Veskai maka akan diberlakukan Item Replacement </p>
+                            <p>Refferal Fee yang ada pada Veskai sudah merupakan persetujuan kedua belah pihak Veskai maupun user, dan tidak dapat diubah.</p>
+                            <p>Refund tidak dapat diberlakukan dengan alasan apapun dikarenakan ini termasuk kesalahan user, jikalau merupakan kesalahan pihak Veskai maka akan diberlakukan Item Replacement. </p>
                         </div>
                         <div class="item" id="termssupportordevelop">
                             <h6 class="title">Support Or Develop</h6>
-                            <p>Support team dapat dilakukan mengirim ke-Email resmi kami yaitu admin@veskai.com, selain dari ini bukan tanggung jawab kami, user juga dilarang mengirim email secara spam, atau akan kami lakukan blok email secara permanent</p>
+                            <p>Support team dapat dilakukan mengirim ke-Email resmi kami yaitu staff@veskai.com, selain dari ini bukan tanggung jawab kami, user juga dilarang mengirim email secara spam, atau akan kami lakukan blok email secara permanent</p>
                             <p>Segala jenis Development environment, code base, version control, debugging, deployment, framework, API, Database, dan lain sebagainya merupakan tanggung jawab kami, dan hal - hal mengenai kami yang berada diluar dari Veskai Production bukan tanggung jawab kami</p>
                         </div>
                     </div>
@@ -163,6 +87,6 @@
             </div>
         </div>
     </div>
-    <!-- ========Privacy Section Ends Here======== -->
+     <!-- ========Privacy Section Ends Here=======  = -->
 
 @endsection
